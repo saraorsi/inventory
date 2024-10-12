18e69ef9904d4b3f29675f5f6fa51e969f5a9a68
saraorsi <saraorsi@Saras-MBP.lan>
2024-10-12 11:38:38 +0100

    correct ts error env

--- app/actions/fetchSheetData.ts
+++ app/actions/fetchSheetData.ts
@@ -1,15 +1,27 @@
 "use server";
 import { google } from "googleapis";
 
-export async function getSheetData() {
+const getEnvVariable = (varName: string): string => {
+  const value = process.env[varName];
+  if (!value) {
+    throw new Error(`${varName} is not defined`);
+  }
+  return value;
+};
+
+interface SheetData {
+  data: string[][];
+}
+
+export async function getSheetData(): Promise<SheetData> {
   const glAuth = await google.auth.getClient({
-    projectId: process.env.PROJECT_ID,
+    projectId: getEnvVariable("PROJECT_ID"),
     credentials: {
       type: "service_account",
-      project_id: process.env.PROJECT_ID,
-      private_key_id: process.env.PRIVATE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.CLIENT_EMAIL,
+      project_id: getEnvVariable("PROJECT_ID"),
+      private_key_id: getEnvVariable("PRIVATE_KEY_ID"),
+      private_key: getEnvVariable("PRIVATE_KEY").replace(/\\n/g, "\n"),
+      client_email: getEnvVariable("CLIENT_EMAIL"),
       universe_domain: "googleapis.com",
     },
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
@@ -17,13 +29,18 @@ export async function getSheetData() {
 
   const glSheets = google.sheets({ version: "v4", auth: glAuth });
 
-  const spreadsheetId = process.env.SHEET_ID;
+  const spreadsheetId = getEnvVariable("SHEET_ID");
   const range = "Teste";
 
-  const data = await glSheets.spreadsheets.values.get({
-    spreadsheetId,
-    range,
-  });
+  try {
+    const response = await glSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
 
-  return { data: data.data.values };
+    return { data: response.data.values || [] };
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    throw new Error("Failed to fetch data from Google Sheets.");
+  }
 }
